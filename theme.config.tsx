a11cf023419f056aaf8bd9b013cc024e324e799b
Jo Seung Hyo <seunghyo7742@naver.com>
2024-06-21 17:36:24 +0900

    init

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Kimjang Wiki</span>,
+  project: {
+    link: 'https://github.com/kimjang-toolkit',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
